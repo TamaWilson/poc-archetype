a904b072a517a60111e4609ebf47734e0e68aaa3
Wilson <wilson.ferreira-ext@viavarejo.com.br>
2022-07-05 17:55:54 -0300

    🐛 Fixing createCommand string and installCommand

--- poc.py
+++ poc.py
@@ -6,9 +6,8 @@ artifactId = input("Digite o ArtifactId: ")
 lombok = "true" if input("Incluir Lombok? (S/N) ").lower() == "s" else "false"
 postgres = "true" if input("Incluir Postgres? (S/N) ").lower() == "s" else "false"
 
-installComannd = "mvn clean install"
-createCommand = f"""mvn archetype:generate "-DoutputDirectory=../" "-DinteractiveMode=false" "-DarchetypeGroupId=br.com.tamawilson" "-DarchetypeArtifactId=poc-archetype" "-DarchetypeVersion=1.0-SNAPSHOT" "-DgroupId={groupId}" "-DartifactId={artifactId}" "-Dlombok={lombok}" "-Dpostgres={postgres}" "-DappName={appName}"""
+createCommand = f'''mvn archetype:generate "-DoutputDirectory=../" "-DinteractiveMode=false" "-DarchetypeGroupId=br.com.tamawilson" "-DarchetypeArtifactId=poc-archetype" "-DarchetypeVersion=1.0-SNAPSHOT" "-DgroupId={groupId}" "-DartifactId={artifactId}" "-Dlombok={lombok}" "-Dpostgres={postgres}" "-DappName={appName}"'''
 
-os.install(f"{installComannd}")
+os.system("mvn clean install")
 print(createCommand)
 os.system(f"{createCommand}")
